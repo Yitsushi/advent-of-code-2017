94ae271f6cc895020bdf7870d709420f1dafd686
Balazs Nadasdi <balazs.nadasdi@cheppers.com>
2017-12-16 21:44:07 +0100

    day-16

---
+++ day-16/part2/solve.php
@@ -0,0 +1,63 @@
+<?php
+
+class DanceFloor {
+  var $abc;
+
+  public function __construct() {
+    $this->abc = 'abcdefghijklmnop';
+  }
+
+  public function spin(int $num) {
+    $this->abc = substr(
+      $this->abc,
+      strlen($this->abc) - $num,
+      strlen($this->abc)
+    ) . substr(
+      $this->abc,
+      0,
+      strlen($this->abc) - $num
+    );
+  }
+
+  public function exchange(int $indexA, int $indexB) {
+    $tmp = $this->abc[$indexA];
+    $this->abc[$indexA] = $this->abc[$indexB];
+    $this->abc[$indexB] = $tmp;
+  }
+
+  public function partner(string $a, string $b) {
+    $this->exchange(strpos($this->abc, $a), strpos($this->abc, $b));
+  }
+}
+
+
+$commands = explode(',', trim(file_get_contents($argv[1])));
+
+$df = new DanceFloor();
+$patterns[] = $df->abc;
+$target = 1000000000;
+for ($i = 0; $i < $target; $i++) {
+  foreach ($commands as $command) {
+    switch ($command[0]) {
+    case 's':
+      list($num) = sscanf($command, 's%d');
+      $df->spin($num);
+      break;
+    case 'x':
+      list($ia, $ib) = sscanf($command, 'x%d/%d');
+      $df->exchange($ia, $ib);
+      break;
+    case 'p':
+      list($a, $b) = sscanf($command, 'p%c/%c');
+      $df->partner($a, $b);
+      break;
+    }
+  }
+  if (in_array($df->abc, $patterns)) {
+    break;
+  }
+
+  $patterns[] = $df->abc;
+}
+
+echo $patterns[($target) % count($patterns)], "\n";
